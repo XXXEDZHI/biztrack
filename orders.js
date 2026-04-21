6e84bd37b9e52abe8c848b8eb236036d7ab63416
Yjw0927 <96541981+Yjw0927@users.noreply.github.com>
2026-04-21 17:29:49 +0800

    Update orders.js

--- orders.js
+++ orders.js
@@ -22,6 +22,31 @@ function closeForm() {
     document.getElementById("order-form").style.display = "none";
 }
 
+function getDecimalPlaces(num) {
+    const str = num.toString();
+    const dotIndex = str.indexOf('.');
+    return dotIndex === -1 ? 0 : str.length - dotIndex - 1;
+}
+
+function safeAdd(a, b) {
+    const maxPlaces = Math.max(getDecimalPlaces(a), getDecimalPlaces(b));
+    const factor = Math.pow(10, maxPlaces);
+    return (a * factor + b * factor) / factor;
+}
+
+function safeMultiply(a, b) {
+    const decimalPlacesA = getDecimalPlaces(a);
+    const decimalPlacesB = getDecimalPlaces(b);
+    const factor = Math.pow(10, decimalPlacesA + decimalPlacesB);
+    return (a * b * factor) / factor;
+}
+
+function safeSubtract(a, b) {
+    const maxPlaces = Math.max(getDecimalPlaces(a), getDecimalPlaces(b));
+    const factor = Math.pow(10, maxPlaces);
+    return (a * factor - b * factor) / factor;
+}
+
 let orders = [];
 
 window.onload = function () {
